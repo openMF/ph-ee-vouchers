3c359e07ec86fe1c31fc388c865d2a872412e170
Ankita Ranjan <ankita@fynarfin.io>
2023-09-01 18:58:00 +0530

    GOV-556 : API Compliance

--- src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/VoucherLifecycleManagementApi.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/VoucherLifecycleManagementApi.java
@@ -1,12 +1,18 @@
 package org.mifos.pheevouchermanagementsystem.api.definition;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.http.ResponseEntity;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.mifos.pheevouchermanagementsystem.data.RequestDTO;
+import org.mifos.pheevouchermanagementsystem.data.ResponseDTO;
+s
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
-
+@Tag(name = "GOV")
 public interface VoucherLifecycleManagementApi {
+    @Operation(
+            summary = "Create Vouchers API")
     @PutMapping("/vouchers")
     <T> ResponseEntity<T> voucherStatusChange(@RequestHeader(value="X-CallbackURL") String callbackURL, @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId,
                                                     @RequestBody Object requestBody, @RequestParam(value = "command") String command) throws ExecutionException, InterruptedException, JsonProcessingException;
