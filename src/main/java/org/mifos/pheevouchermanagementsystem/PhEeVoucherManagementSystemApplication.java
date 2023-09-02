3814836bb2a22901822ed9c6390ad0fc21ce25d8
Ankita Ranjan <92036361+ankita10r@users.noreply.github.com>
2023-09-02 15:23:45 +0530

    GOV-556 : API Compliance (#7)

--- src/main/java/org/mifos/pheevouchermanagementsystem/PhEeVoucherManagementSystemApplication.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/PhEeVoucherManagementSystemApplication.java
@@ -2,8 +2,12 @@ package org.mifos.pheevouchermanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableCaching
 @SpringBootApplication
+@ComponentScan("org.mifos.pheevouchermanagementsystem")
 public class PhEeVoucherManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/CreateVoucherApi.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/CreateVoucherApi.java
@@ -1,6 +1,8 @@
 package org.mifos.pheevouchermanagementsystem.api.definition;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.mifos.pheevouchermanagementsystem.data.RequestDTO;
 import org.mifos.pheevouchermanagementsystem.data.ResponseDTO;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.concurrent.ExecutionException;
-
+@Tag(name = "GOV")
 public interface CreateVoucherApi {
+    @Operation(
+            summary = "Create Vouchers API")
     @PostMapping("/vouchers")
     ResponseEntity<ResponseDTO> createVouchers(@RequestHeader(value="X-CallbackURL") String callbackURL, @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId,
                                                @RequestBody RequestDTO requestBody) throws ExecutionException, InterruptedException, JsonProcessingException;
