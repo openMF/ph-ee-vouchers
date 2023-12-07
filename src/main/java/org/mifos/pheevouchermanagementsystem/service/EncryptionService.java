0280662adcb7cc923fb16556bdb75c3675870eeb
DanishJamal <danishjamal.104@gmail.com>
2023-12-07 19:13:13 +0530

    Build fixed

--- src/main/java/org/mifos/pheevouchermanagementsystem/service/EncryptionService.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/service/EncryptionService.java
@@ -1,14 +1,14 @@
 package org.mifos.pheevouchermanagementsystem.service;
 
-import org.mifos.connector.common.util.SecurityUtil;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import org.mifos.connector.common.util.SecurityUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 @Service
 public class EncryptionService {
@@ -19,13 +19,13 @@ public class EncryptionService {
     @Value("${rsa-key.x509Certificate}")
     private String publicKey;
 
-    public String encrypt(String data) throws NoSuchPaddingException, IllegalBlockSizeException,
-            NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+    public String encrypt(String data) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+            BadPaddingException, InvalidKeySpecException, InvalidKeyException {
         return SecurityUtil.encryptUsingPrivateKey(data, privateKey);
     }
 
-    public String decrypt(String encryptedData) throws NoSuchPaddingException, IllegalBlockSizeException,
-            NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+    public String decrypt(String encryptedData) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+            BadPaddingException, InvalidKeySpecException, InvalidKeyException {
         return SecurityUtil.decryptUsingPublicKey(encryptedData, publicKey);
     }
 
--- src/main/java/org/mifos/pheevouchermanagementsystem/util/EncryptVoucher.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/util/EncryptVoucher.java
@@ -1,8 +1,5 @@
 package org.mifos.pheevouchermanagementsystem.util;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
