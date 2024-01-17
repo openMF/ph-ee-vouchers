66ffdec64d7cbf2ffb04935887045d2809f5773c
Sourabh Kumar <148945054+shkr7@users.noreply.github.com>
2024-01-17 17:58:43 +0530

    [PHEE-521] Fix for BMPN 500 issue (#19)
    * Fix for BMPN 500 issue
    
    * Fix checkstyle issue
    
    * Added Exception Mapper
    
    * Fixed exception mapper
    
    * Fixed exception mapper
    
    * Added enums
    
    * Fixed checkstyle issue
    
    ---------
    
    Co-authored-by: Sourabh <sourabh@Sourabhs-MacBook-Pro.local>

--- src/main/java/org/mifos/pheevouchermanagementsystem/api/implementation/CreateVoucherApiController.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/api/implementation/CreateVoucherApiController.java
@@ -1,10 +1,8 @@
 package org.mifos.pheevouchermanagementsystem.api.implementation;
 
-import static org.mifos.pheevouchermanagementsystem.util.VoucherManagementEnum.FAILED_RESPONSE;
 import static org.mifos.pheevouchermanagementsystem.util.VoucherManagementEnum.SUCCESS_RESPONSE;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.concurrent.ExecutionException;
+import lombok.extern.slf4j.Slf4j;
 import org.mifos.pheevouchermanagementsystem.api.definition.CreateVoucherApi;
 import org.mifos.pheevouchermanagementsystem.data.RequestDTO;
 import org.mifos.pheevouchermanagementsystem.data.ResponseDTO;
@@ -15,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CreateVoucherApiController implements CreateVoucherApi {
 
     @Autowired
@@ -22,13 +21,8 @@ public class CreateVoucherApiController implements CreateVoucherApi {
 
     @Override
     public ResponseEntity<ResponseDTO> createVouchers(String callbackURL, String programId, String registeringInstitutionId,
-            RequestDTO requestBody) throws ExecutionException, InterruptedException, JsonProcessingException {
-        try {
-            createVoucherService.createVouchers(requestBody, callbackURL, registeringInstitutionId);
-        } catch (Exception e) {
-            ResponseDTO responseDTO = new ResponseDTO(FAILED_RESPONSE.getValue(), FAILED_RESPONSE.getMessage(), requestBody.getRequestID());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
-        }
+                                                      RequestDTO requestBody) {
+        createVoucherService.createVouchers(requestBody, callbackURL, registeringInstitutionId);
         ResponseDTO responseDTO = new ResponseDTO(SUCCESS_RESPONSE.getValue(), SUCCESS_RESPONSE.getMessage(), requestBody.getRequestID());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDTO);
     }
---
+++ src/main/java/org/mifos/pheevouchermanagementsystem/exception/ZeebeClientStatusException.java
@@ -0,0 +1,14 @@
+package org.mifos.pheevouchermanagementsystem.exception;
+
+public class ZeebeClientStatusException extends RuntimeException {
+    private final String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public ZeebeClientStatusException(String message, String id, Throwable cause) {
+        super(message);
+        this.id = id;
+    }
+}
