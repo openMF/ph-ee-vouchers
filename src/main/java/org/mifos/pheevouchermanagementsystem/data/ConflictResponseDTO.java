26bb93b626b7a146f412d1e3595eca6cc16b3ee6
sourabh <sourabh@fynarfin.io>
2024-03-12 03:04:05 +0530

    [PHEE-616] Fix InstructionID conflict issue in voucher

---
+++ src/main/java/org/mifos/pheevouchermanagementsystem/data/ConflictResponseDTO.java
@@ -0,0 +1,18 @@
+package org.mifos.pheevouchermanagementsystem.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConflictResponseDTO {
+
+    private String responseCode;
+    private String responseDescription;
+    private String conflictingFieldName;
+    private String conflictingFieldValue;
+}
