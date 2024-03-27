7d5310564b61ec93fe524c5cf4234523b985910f
sourabh <sourabh@fynarfin.io>
2024-03-27 14:52:32 +0530

    Added seperate API interface and resolved mapping issue

--- src/main/java/org/mifos/pheevouchermanagementsystem/api/implementation/CancelAndRedeemVoucherApiController.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/api/implementation/CancelAndRedeemVoucherApiController.java
@@ -5,7 +5,7 @@ import static org.mifos.pheevouchermanagementsystem.util.VoucherManagementEnum.S
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mifos.connector.common.channel.dto.PhErrorDTO;
-import org.mifos.pheevouchermanagementsystem.api.definition.VoucherLifecycleManagementApi;
+import org.mifos.pheevouchermanagementsystem.api.definition.CancelAndRedeemVoucherApi;
 import org.mifos.pheevouchermanagementsystem.data.RedeemVoucherRequestDTO;
 import org.mifos.pheevouchermanagementsystem.data.RequestDTO;
 import org.mifos.pheevouchermanagementsystem.data.ResponseDTO;
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class VoucherLifecycleManagementApiController implements VoucherLifecycleManagementApi {
+public class CancelAndRedeemVoucherApiController implements CancelAndRedeemVoucherApi {
 
     @Autowired
     ActivateVoucherService activateVoucherService;
@@ -34,45 +34,32 @@ public class VoucherLifecycleManagementApiController implements VoucherLifecycle
     VoucherValidator voucherValidator;
 
     @Override
-    public <T> ResponseEntity<T> voucherStatusChange(String callbackURL, String registeringInstitutionId, String programId,
+    public <T> ResponseEntity<T> cancelOrRedeemVoucher(String callbackURL, String registeringInstitutionId, String programId,
             Object requestBody, String command) {
         RequestDTO requestDTO = null;
         RedeemVoucherRequestDTO redeemVoucherRequestDTO = null;
         try {
-            if (command.equals("activate")) {
-                requestDTO = objectMapper.convertValue(requestBody, RequestDTO.class);
-                PhErrorDTO phErrorDTO = voucherValidator.validateVoucherLifecycle(requestDTO);
-                if (phErrorDTO != null) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) phErrorDTO);
-                }
-                activateVoucherService.activateVouchers(requestDTO, callbackURL, registeringInstitutionId);
-            } else if (command.equals("suspend")) {
-                requestDTO = objectMapper.convertValue(requestBody, RequestDTO.class);
-                PhErrorDTO phErrorDTO = voucherValidator.validateVoucherLifecycle(requestDTO);
-                if (phErrorDTO != null) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) phErrorDTO);
-                }
-                voucherLifecycleService.suspendVoucher(requestDTO, callbackURL, registeringInstitutionId);
-            } else if (command.equals("reactivate")) {
-                requestDTO = objectMapper.convertValue(requestBody, RequestDTO.class);
-                PhErrorDTO phErrorDTO = voucherValidator.validateVoucherLifecycle(requestDTO);
-                if (phErrorDTO != null) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) phErrorDTO);
-                }
-                voucherLifecycleService.reactivateVoucher(requestDTO, callbackURL, registeringInstitutionId);
-            } else if (command.equals("redeemPay")) {
+            if (command.equals("redeem")) {
                 redeemVoucherRequestDTO = objectMapper.convertValue(requestBody, RedeemVoucherRequestDTO.class);
-
                 PhErrorDTO phErrorDTO = voucherValidator.validateRedeemVoucher(redeemVoucherRequestDTO);
                 if (phErrorDTO != null) {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) phErrorDTO);
                 }
-                redeemVoucherService.redeemAndPay(redeemVoucherRequestDTO, callbackURL, registeringInstitutionId);
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body((T) redeemVoucherService.redeemVoucher(redeemVoucherRequestDTO, registeringInstitutionId));
+            } else if (command.equals("cancel")) {
+                requestDTO = objectMapper.convertValue(requestBody, RequestDTO.class);
+                PhErrorDTO phErrorDTO = voucherValidator.validateVoucherLifecycle(requestDTO);
+                if (phErrorDTO != null) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body((T) phErrorDTO);
+                }
+                voucherLifecycleService.cancelVoucher(requestDTO, callbackURL, registeringInstitutionId);
             }
         } catch (Exception e) {
             ResponseDTO responseDTO = new ResponseDTO(FAILED_RESPONSE.getValue(), FAILED_RESPONSE.getMessage(), requestDTO.getRequestID());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body((T) responseDTO);
         }
+
         String requestId;
         if (requestDTO == null) {
             requestId = redeemVoucherRequestDTO.getRequestId();
@@ -81,6 +68,5 @@ public class VoucherLifecycleManagementApiController implements VoucherLifecycle
         }
         ResponseDTO responseDTO = new ResponseDTO(SUCCESS_RESPONSE.getValue(), SUCCESS_RESPONSE.getMessage(), requestId);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body((T) responseDTO);
-
     }
 }
