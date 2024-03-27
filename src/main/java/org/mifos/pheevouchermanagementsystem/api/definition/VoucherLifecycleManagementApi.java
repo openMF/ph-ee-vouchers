4c87c6bad6d9ee9df3dcab875e1e0169cf8e32cb
sourabh <sourabh@fynarfin.io>
2024-03-27 14:52:32 +0530

    Added seperate API interface and resolved mapping issue

--- src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/VoucherLifecycleManagementApi.java
+++ src/main/java/org/mifos/pheevouchermanagementsystem/api/definition/VoucherLifecycleManagementApi.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.concurrent.ExecutionException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,13 +16,6 @@ public interface VoucherLifecycleManagementApi {
     @Operation(summary = "Create Vouchers API")
     @PutMapping("/vouchers")
     <T> ResponseEntity<T> voucherStatusChange(@RequestHeader(value = "X-CallbackURL") String callbackURL,
-            @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId,
-            @RequestHeader(value = "X-Program-ID", required = false) String programId, @RequestBody Object requestBody,
-            @RequestParam(value = "command") String command)
-            throws ExecutionException, InterruptedException, JsonProcessingException, JsonProcessingException;
-
-    @PostMapping("/vouchers")
-    <T> ResponseEntity<T> activateVoucherChange(@RequestHeader(value = "X-CallbackURL") String callbackURL,
             @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId,
             @RequestHeader(value = "X-Program-ID", required = false) String programId, @RequestBody Object requestBody,
             @RequestParam(value = "command") String command)
